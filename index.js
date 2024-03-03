5d59cac79eb8e44dde145a4d1410c10aaa8d0be0
burt reynolds <jnelson4004@gmail.com>
2024-03-03 13:21:25 -0600

    init commit with packages installed

---
+++ index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [title, sections, tableOfContents, installation, usage, license, contributing, tests];
+    this.title = title;
+    this.sections = sections;
+    this.tableOfContents = tableOfContents;
+    this.installation = installation;
+    this.usage = usage;
+    this.license = license;
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
