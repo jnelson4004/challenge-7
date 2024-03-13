f1e8e174bf101bdead478cf63e76f4b208b346a3
burt reynolds <jnelson4004@gmail.com>
2024-03-12 23:13:31 -0500

    working form editor

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [title, sections, tableOfContents, installation, usage, license, contributing, tests];
-    this.title = title;
-    this.sections = sections;
-    this.tableOfContents = tableOfContents;
-    this.installation = installation;
-    this.usage = usage;
-    this.license = license;
+inquirer.prompt([
+    {
+        type: "input",
+        name: "Title",
+        message: "Enter your title:"
+    },
+    {
+        type: "input",
+        name: "Table_of_Contents",
+        message: "Enter a table of contents:"
+    },
+    {   
+        type: "input",
+        name: "Installation",
+        message: "Enter instructions for installation:"
+    },
+    {   
+        type: "input",
+        name: "Usage",
+        message: "Enter instructions for usage:"
+    },
+    {   
+        type: "input",
+        name: "License",
+        message: "Enter license"
+    },
+    {   
+        type: "input",
+        name: "Contributing",
+        message: "Enter names of any contributing partners"
+    },
+    {   
+        type: "input",
+        name: "Tests",
+        message: "Enter a description of any tests run on the program"
+    },
+    {   
+        type: "input",
+        name: "Questions",
+        message: "Enter any questions"
+    }
+
+]).then(answers => {
+    console.log('User input:');
+    console.log(answers);
+});
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, answers) {};
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
+
+//1. program must accept user data from the command line
+//    a. call program (node {nameofprogram.js})
+//    b. user is asked a question through inquirer package, response saved
+//        -how to use inquirer to ask question (read documentation)
+//2. transform data into predetermined format
+//3. create file containing data
